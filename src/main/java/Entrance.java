f94ed64ce82f6a6cfc0f2412c3fb56aad57f38da
fei-ke <jinyang656@qq.com>
2018-04-06 14:29:50 +0800

    Support directory recursively decode/encode

--- src/main/java/Entrance.java
+++ src/main/java/Entrance.java
@@ -3,37 +3,39 @@ import org.apache.commons.cli.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipException;
 
 public class Entrance {
-    public static void main(String[] args) throws ParseException, IOException {
+    private static OmcTextDecoder decoder = new OmcTextDecoder();
+
+    public static void main(String[] args) throws ParseException {
         Options options = new Options();
 
         options.addOption("h", "help", false, "print this message");
         options.addOption("e", "encode", false, "encode omc file");
         options.addOption("d", "decode", false, "decode omc file");
-        options.addOption("i", "input", true, "input file");
-        options.addOption("o", "output", true, "output file");
+        options.addOption("i", "input", true, "input file or dir");
+        options.addOption("o", "output", true, "output file or dir");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
 
 
-        File inputFile = null;
+        File inputFile;
         File outputFile;
 
         boolean decode = true;
 
         if (cmd.hasOption('h') || args.length == 0) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("java -jar <the jar name> -i cscfeture.xml -o cscfeture_decoded.xml", options);
+            printHelp(options);
             return;
         }
 
         if (cmd.hasOption('i')) {
             inputFile = new File(cmd.getOptionValue("i"));
         } else {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("java -jar <the jar name> -i cscfeture.xml -o cscfeture_decoded.xml", options);
+            printHelp(options);
+            return;
         }
         if (cmd.hasOption("o")) {
             outputFile = new File(cmd.getOptionValue('o'));
@@ -45,17 +47,57 @@ public class Entrance {
             decode = false;
         }
 
-        OmcTextDecoder decoder = new OmcTextDecoder();
-        byte[] bytes;
-        if (decode)
-            bytes = decoder.decode(inputFile);
-        else {
-            bytes = decoder.encode(inputFile);
+        decode(inputFile, outputFile, decode);
+    }
+
+    private static void decode(File in, File out, boolean decode) {
+        if (in.isDirectory()) {
+            File[] files = in.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    decode(file, new File(out, file.getName()), decode);
+                }
+            }
+        } else {
+            decodeFile(in, out, decode);
         }
-        FileOutputStream outputStream = new FileOutputStream(outputFile);
-        outputStream.write(bytes);
-        outputStream.close();
-        System.out.println("output " + outputFile.getName() + " success");
+    }
+
+    private static void decodeFile(File inputFile, File outputFile, boolean decode) {
+        try {
+            String path = inputFile.getAbsolutePath();
+            if (!path.endsWith(".xml")) {
+                //not xml file, ignored
+                return;
+            }
+
+            byte[] bytes;
+            if (decode)
+                bytes = decoder.decode(inputFile);
+            else {
+                bytes = decoder.encode(inputFile);
+            }
+
+            if (outputFile.exists()) {
+                outputFile.delete();
+            }
+            outputFile.getParentFile().mkdirs();
+            outputFile.createNewFile();
+
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(bytes);
+            outputStream.close();
+            System.out.println("output " + outputFile.getAbsolutePath() + " success");
+        } catch (ZipException e) {
+            //
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar <the jar name> -i cscfeture.xml -o cscfeture_decoded.xml", options);
     }
 }
 
